df72f38b0f9be76e598a73b2c9e235b2c34dd804
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-06-16 02:57:43 +0000

    Automatically backed up by Learn

--- app/services/sighting_serializer.rb
+++ app/services/sighting_serializer.rb
@@ -8,15 +8,15 @@ class SightingSerializer
     options = {
       include: {
         bird: {
-            only: [:name, :species]
-          },
-          location: {
-            only: [:latitude, :longitude]
-          }
+          only: [:name, :species]
         },
-        except: [:updated_at],
-      }
-      @sighting.to_json(options)
-    end
+        location: {
+          only: [:latitude, :longitude]
+        }
+      },
+      except: [:updated_at],
+    }
+    @sighting.to_json(options)
+  end
   
 end
